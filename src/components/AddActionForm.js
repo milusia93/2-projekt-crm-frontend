7354d3a006215831c397e928e21ef58ff41bea1a
MilenaD <44272975+milusia93@users.noreply.github.com>
2024-11-22 15:21:23 +0100

    action in pro

--- src/components/AddActionForm.js
+++ src/components/AddActionForm.js
@@ -1,9 +1,26 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import { pl } from 'date-fns/locale/pl';
+registerLocale('pl', pl)
 const AddActionForm = (props) => {
+    const [startDate, setStartDate] = useState(new Date());
 return (
     <div className="formwrapper">
         <form>
-            
+            <div className="wrapper">
+            <label htmlFor="date">Data</label>
+            <DatePicker locale="pl" id="date" selected={startDate} onChange={(date) => setStartDate(date)} />
+            </div>
+            <div>
+            <label htmlFor="actiontype">Typ akcji</label>
+            </div>
+            <div>
+            <label htmlFor="description">Opis</label>
+            </div>
         </form>
     </div>
 )
 }
+export default AddActionForm; 
--- src/views/AddAction.js
+++ src/views/AddAction.js
@@ -1,7 +1,75 @@
-const AddAction = () => {
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import SingleClientTable from "../components/SingleClientTable";
+import AddActionForm from "../components/AddActionForm";
+
+const AddAction = (props) => {
+    const params = useParams();
+    const id = params.id;
+
+    const [singleClient, setSingleClient] = useState({
+        name: "",
+        address: {
+            street: "",
+            zipCode: "",
+            city: "",
+        },
+        nip: "",
+    });
+
+    // const [addedAction, setAddedAction] = useState({
+    //     date: "",
+    //     actionType: "",
+    //     description: "",
+    // });
+    
+    // const [errors, setErrors] = useState({
+    //     date: "",
+    //     actionType: "",
+    //     description: "",
+    // })
+
+    const getSingleClient = () => {
+        axios
+            .get(`http://localhost:3005/clients/${id}`)
+            .then((res) => {
+                setSingleClient(res.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
+    useEffect(() => {
+        if (id) {
+            getSingleClient();
+        }
+
+        return () => {
+            resetForm()
+        }
+    }, [id]);
+
+    const resetForm = () => {
+        setSingleClient({
+            name: "",
+            address: {
+                street: "",
+                zipCode: "",
+                city: "",
+            },
+            nip: "",
+        });
+    };
     return (
         <div>
-            <h2>AddAction</h2>
+            <div className="tableContainer">
+                <SingleClientTable client={singleClient} />
+            </div>
+            <div className="formContainer">
+                <AddActionForm />
+            </div>
         </div>
 
     )
